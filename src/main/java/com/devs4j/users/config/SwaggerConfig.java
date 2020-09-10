f3ea56533ea8aa9f07ac770dae3adc92d80e1a41
Alejandro Agapito - maagapi <Alejandro.Agapito@walmart.com>
2020-09-10 09:10:52 -0500

    move back swagger version

--- src/main/java/com/devs4j/users/config/SwaggerConfig.java
+++ src/main/java/com/devs4j/users/config/SwaggerConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -15,7 +14,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@EnableOpenApi
 public class SwaggerConfig {
 	
 	
